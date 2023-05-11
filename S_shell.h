e475739bb8490302bc4bb5a937e2921081b9c0f2
Joram21 <oloojoram@gmail.com>
2023-05-11 16:01:20 -0700

    header

---
+++ S_shell.h
@@ -0,0 +1,12 @@
+/* HEADER GUARDS*/
+#ifndef _shell_
+#define _shell_
+
+/*including lib to be used*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdarg.h>
